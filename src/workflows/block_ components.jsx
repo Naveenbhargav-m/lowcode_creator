eb93c7df446efe1260164435ce2e3fea8b30a8ce
Naveenbhargav-Mallampati <141256654+Naveenbhargav-Mallampati@users.noreply.github.com>
2025-03-06 22:29:59 +0530

    workflow view is in progress

--- src/workflows/block_ components.jsx
+++ src/workflows/block_ components.jsx
@@ -1,7 +1,8 @@
+import { NodeResizer } from "@xyflow/react";
 import DynamicIcon from "../components/custom/dynamic_icon";
 
 
-function InsertRow() {
+export function InsertRow({data, isConnectable}) {
     let name = "Insert Row";
     let icon = "database";
     let  style={"display":"flex", "flexDirection":"row",backgroundColor:"black", "fontSize":"0.8em",margin:"10px 0px", padding:"18px", borderRadius:"20px"};
@@ -14,7 +15,7 @@ function InsertRow() {
     );
 }
 
-function UpdateRow() {
+export function UpdateRow({data, isConnectable}) {
     let name = "Update Row";
     let icon = "database";
     let  style={"display":"flex", "flexDirection":"row",backgroundColor:"black", "fontSize":"0.8em",margin:"10px 0px", padding:"18px", borderRadius:"20px"};
@@ -27,13 +28,41 @@ function UpdateRow() {
     );
 }
 
-function Condition() {
+export function Condition({data, isConnectable}) {
     let name = "condition";
     let icon = "parentheses";
     let  style={"display":"flex", "flexDirection":"row",backgroundColor:"black", "fontSize":"0.8em",margin:"10px 0px", padding:"18px", borderRadius:"20px"};
 
     return (
         <div style={style}>
+            <div style={{padding:"20px"}}><DynamicIcon name={icon} size={20}/></div>
+            {name}
+        </div>
+    );
+}
+
+export function Start({data, isConnectable}) {
+    let name = "condition";
+    let icon = "parentheses";
+    let  style={"display":"flex", "flexDirection":"row",backgroundColor:"black", "fontSize":"0.8em",margin:"10px 0px", padding:"18px", borderRadius:"20px"};
+
+    return (
+        <div style={style}>
+            <NodeResizer />
+            <div style={{padding:"20px"}}><DynamicIcon name={icon} size={20}/></div>
+            {name}
+        </div>
+    );
+}
+
+export function End({data, isConnectable}) {
+    let name = "condition";
+    let icon = "parentheses";
+    let  style={"display":"flex", "flexDirection":"row",backgroundColor:"black", "fontSize":"0.8em",margin:"10px 0px", padding:"18px", borderRadius:"20px"};
+
+    return (
+        <div style={style}>
+            <NodeResizer />
             <div style={{padding:"20px"}}><DynamicIcon name={icon} size={20}/></div>
             {name}
         </div>
