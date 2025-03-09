c95c5e0d57bba592e2671c4e9bfb0c9ebd95dfbd
Naveenbhargav-Mallampati <141256654+Naveenbhargav-Mallampati@users.noreply.github.com>
2025-03-09 12:24:15 +0530

    edges and nodes positions are stable now

--- src/workflows/flow_builder.jsx
+++ src/workflows/flow_builder.jsx
@@ -1,5 +1,5 @@
-import { addEdge, applyEdgeChanges, applyNodeChanges, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
-import { activeWorkFlow, HandleWorkFlowBlockDrop } from "./workflow_state";
+import { addEdge, ReactFlow, useEdgesState, useNodesState } from "@xyflow/react";
+import { activeFloweUpdated, activeWorkFlow, HandleWorkFlowBlockDrop, UpdateActiveWorkflowEdges, UpdateActiveWorkflowNodes } from "./workflow_state";
 import { useCallback, useEffect, useState } from "preact/hooks";
 import { Condition, End, InsertRow, Start, UpdateRow } from "./block_ components";
 import { Drop } from "../components/custom/Drop";
@@ -17,33 +17,32 @@ let nodeTypes = {
 function FlowBuilder() {
   
   const [edges, setEdges, onEdgesChange] = useEdgesState([]);
-  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [nodes, setNodes , onNodesChange] = useNodesState([]);
 
   useEffect(() => {
     const activeFlow = activeWorkFlow.value || { nodes: [], edges: [] };
     console.log("active flow:",activeFlow);
     setNodes(activeFlow["nodes"]);
     setEdges(activeFlow["edges"]);
-  }, [activeWorkFlow.value]);
+  }, [activeFloweUpdated.value]);
 
-      // Effect to call other functions when nodes or edges change
-      useEffect(() => {
-        // Call your function to update the config of workflows here
-        console.log("called the nodes updation callback", nodes);
-        // updateWorkflowConfig(nodes, edges);
-      }, [nodes]);
 
-      useEffect(() => {
-        console.log("called the edges updation callback", edges);
-      },[edges])
+  useEffect(() => {
+    UpdateActiveWorkflowNodes(nodes);
+  }, [nodes]);
 
-      const onConnect = useCallback(
+  useEffect(() => {
+    UpdateActiveWorkflowEdges(edges);
+  }, [edges]);
+
+
+  const onConnect = useCallback(
         (params) => {
           console.log("Connection params:", params); // Debugging log
           setEdges((eds) => addEdge(params, eds));
         },
         [],
-      );
+  );
 
 
   return (
@@ -62,6 +61,45 @@ function FlowBuilder() {
     </Drop>
   );
 }
+
+
+// function FlowBuilder() {
+//   console.log("nodes:",activeWorkFlow.value["nodes"]);
+//   console.log("edges:",activeWorkFlow.value["edges"]);
+//   const onNodesChange = useCallback((nodes) => {
+//       console.log("updated nodes:",nodes);
+//       UpdateActiveWorkflowNodes(nodes);
+//   }, []);
+
+//   const onEdgesChange = useCallback((edges) => {
+//     UpdateActiveWorkflowEdges(edges);
+//   }, []);
+
+//   const onConnect = useCallback((params) => {
+//       console.log("on connect params:",params);
+//   }, []);
+
+
+//   return (
+//     <Drop
+//       onDrop={(data) => HandleWorkFlowBlockDrop(data)}
+//       dropElementData={{ element: "screen" }}
+//       wrapParent={true}
+//     >
+//       <div style={{ height: "90vh", width: "70vw" }}>
+//         <ReactFlow
+//           nodes={activeWorkFlow.value.nodes}
+//           edges={activeWorkFlow.value.edges}
+//           onNodesChange={onNodesChange}
+//           onEdgesChange={onEdgesChange}
+//           onConnect={onConnect}
+//           nodeTypes={nodeTypes}
+//           fitView
+//         />
+//       </div>
+//     </Drop>
+//   );
+// }
 
 
 export {FlowBuilder};
