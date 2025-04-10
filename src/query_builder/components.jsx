32d939278db27b5c90b22f5c314fc8f36e204aa0
Naveenbhargav-Mallampati <141256654+Naveenbhargav-Mallampati@users.noreply.github.com>
2025-04-10 22:40:35 +0530

    query block's join and select is implemented

--- src/query_builder/components.jsx
+++ src/query_builder/components.jsx
@@ -6,6 +6,8 @@ import QueryCreator from "./query_builder";
 import { CreateQueryBlock } from "./query_functions";
 import { ActiveQuery, QueryNames } from "./query_signal";
 import { Pipette } from "lucide-react";
+import { useState } from "preact/hooks";
+import { SelectComponent } from "../components/general/general_components";
 
 let headerStyle = { display:"flex", "flexDirection":"row", "justifyContent":"flex-start", "padding":"10px" };
 let iconStyle  = {"padding":"8px",backgroundColor:"black", "color":"white", borderRadius:"10px"};
@@ -54,26 +56,94 @@ export function TablesView({prefilData}) {
 
 function SelectBlock() {
     let isOpen = useSignal(false);
+    const [selectedItems, setSelectedItems] = useState([]);
+    
+    // Handle removing a specific tag
+    const removeTag = (itemToRemove) => {
+        setSelectedItems(prev => prev.filter(item => item !== itemToRemove));
+    };
+    
     return (
-        <div class="section">
-        <BlocksHeader isOpen={isOpen} title={"Select Block"}/>
-        <textarea placeholder="Enter columns to select (e.g., id, name, email)"></textarea>
-        <GlobalSignalsPopup isOpen={isOpen} 
-              onChangeCallback={(e) => {}}
-             closeCallback={(e,data) => {console.log("closed :",data); isOpen.value = false}}/>
+        <div className="section">
+            <BlocksHeader isOpen={isOpen} title={"Select Block"}/>
+            <SelecList selectedItems={selectedItems} removeTag={removeTag}/>
+            <GlobalSignalsPopup 
+                isOpen={isOpen} 
+                onChangeCallback={(e) => {}}
+                closeCallback={(e, data) => {
+                    console.log("closed :", data);
+                    setSelectedItems(data);
+                    isOpen.value = false;
+                }}
+            />
         </div>
     );
 }
 
 function JoinBlock() {
     let isOpen = useSignal(false);
+    let popupField = useSignal("");
+    let popupInd = useSignal(-1);
+    const handlePopupClose = (e, data) => {
+        if(data === undefined) {
+            isOpen.value = false;
+            return;
+        }
+        let last = data.pop();
+        let key = popupField.value;
+        let obj = {};
+        obj[key] = last;
+        UpdateJoinData(obj, popupInd.value);
+        isOpen.value = false;
+    };
+    let buttonStyle = {backgroundColor:"black", "color": "white"};  
+    function UpdateJoinData(obj, index) {
+        console.log("object:",obj, index);
+        var exisitng = joinsData.value;
+        let cur = exisitng[index];
+        cur = {...cur, ...obj};
+        exisitng[index] = cur;
+        joinsData.value = [...exisitng];
+    }
+    let joinsData = useSignal([]);
     return (
-        <div class="section">
-        <BlocksHeader isOpen={isOpen} title={"From-join Block"}/>
-        <textarea placeholder="Enter table(s) and join conditions (e.g., users LEFT JOIN orders ON users.id = orders.user_id)"></textarea>
-        <GlobalSignalsPopup isOpen={isOpen} 
-              onChangeCallback={(e) => {}}
-             closeCallback={(e,data) => {console.log("closed :",data); isOpen.value = false}}/>
+        <div className="section">
+            <BlocksHeader isOpen={isOpen} title={"From-join Block"}/>
+                {
+                    joinsData.value.map((value, index) => {
+                        return (
+                            <div style={{"display":"flex", "flexDirection":"row", "justifyContent": "flex-start", "marginBottom":"8px"}}>
+                            <SelectComponent 
+                            options={["left join", "right join", "join", "inner join", "cross join"]}
+                            onChange={(e,data) => {UpdateJoinData({"join": data["value"]}, index);}}
+                            selected={value["join"] || "left join"}
+                            style={{"width": "120px", "marginRight":"10px"}}
+                            />
+                            <button style={{...buttonStyle, "padding": "8px 10px", "marginRight":"10px"}}
+                            onClick={(e) => {popupField.value = "first_field"; popupInd.value = index; isOpen.value = true}}
+                            >
+                                {value["first_field"] || "first_field"}</button>
+                            <SelectComponent 
+                            options={["equals", "less_than", "greater_than", "less_eq", "greater_eq"]}
+                            onChange={(e,data) => {UpdateJoinData({"operator": data["value"]}, index);}}
+                            selected={value["operator"] || "equals"}
+                            style={{"width": "120px", "marginRight":"10px"}}
+                            />
+                             <button style={{...buttonStyle, "padding": "8px 10px", "marginRight":"10px"}}
+                             onClick={(e) => {popupField.value = "second_field"; popupInd.value = index; isOpen.value = true}}
+                             >{value["second_field"] || "Second_field"}</button>
+                             </div>
+                        );
+                    })
+                }
+            <button style={{...buttonStyle, "padding": "8px 10px", "marginRight":"10px"}}
+            onClick={(e)=> {let cur = joinsData.value; cur.push({}); joinsData.value = [...cur];}}
+            >Add Join</button>
+            <GlobalSignalsPopup 
+                isOpen={isOpen} 
+                onChangeCallback={(e) => {}}
+                closeCallback={handlePopupClose}
+            />
         </div>
     );
 }
@@ -126,5 +196,31 @@ function BlocksHeader({isOpen, title}) {
         style={iconStyle}
         />
     </div>
+    );
+}
+
+
+function SelecList({selectedItems, removeTag}) {
+    return (
+        <div className="p-3 border rounded min-h-12 flex flex-wrap gap-2">
+        {selectedItems.length === 0 ? (
+            <div className="text-gray-400">Click to select columns (e.g., id, name, email)</div>
+        ) : (
+            selectedItems.map((item, index) => (
+                <div key={index} className="px-2 py-1 rounded-md flex items-center text-sm" 
+                style={{backgroundColor:"black", "color":"white","padding":"4px", borderRadius:"10px", "fontSize":"0.6em"}}
+                >
+                    <span >{item}</span>
+                    <button 
+                        className=""
+                        style={{"backgroundColor": "black" , "padding":"4px 8px"}} 
+                        onClick={() => removeTag(item)}
+                    >
+                        ×
+                    </button>
+                </div>
+            ))
+        )}
+    </div>            
     );
 }
