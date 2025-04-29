ab2bdb6cda7fd2ff63e26daf163811002843a3b2
Naveenbhargav-Mallampati <141256654+Naveenbhargav-Mallampati@users.noreply.github.com>
2025-04-29 09:15:44 +0530

    NA

--- src/workflows/blocks_requirements.jsx
+++ src/workflows/blocks_requirements.jsx
@@ -1,31 +1,24 @@
 
 
 let blocksRequirements = {
-    "razor_pay": {
-        "title": "RazorPay (Create Payment Link)",
-        "inputs": [
-          {"title": "Connection", "type": "select", "key": "connection", "required": true, "options": ["Connection 1", "Connection 2"]},
-          {"title": "Amount", "type": "textfield", "key": "amount", "required": true},
-          {"title": "Currency", "type": "select", "key": "currency", "required": true, "options": ["INR", "USD", "EUR"]},
-          {"title": "Reference ID", "type": "textfield", "key": "reference_id"},
-          {"title": "Description", "type": "textarea", "key": "description"},
-          {"title": "Customer Name", "type": "textfield", "key": "customer_name"},
-          {"title": "API Key", "type": "textfield", "key": "api_key"},
-          {"title": "API Secret", "type": "textfield", "key": "api_secret"}
-        ]},
-    "insert_row": {"title": "Insert Row","inputs": [
-          {"title": "Table", "type": "select", "key": "table", "required": true, "options": ["users", "posts"]},
-          {"title": "fields", "type": "map", "key": "fields", "required": true}
-        ] },
-    "update_row": {"title": "Update Row","inputs": [
-          {"title": "Table", "type": "select", "key": "table", "required": true, "options": ["users", "posts"]},
-          {"title": "fields", "type": "map", "key": "fields", "required": true}, 
-          {"title": "filter", "type": "map", "key": "filter", "required": true}
-        ] },
-    "start": {"title": "Start","inputs": [
-          {"title": "IsBackground", "type": "toggle", "key": "is_background", "required": true}
-        ] },
-    "end": {"title": "End","inputs": []},
+   "start": {
+    "label": "start",
+    "description": "This is the start of workflow, that also take global state for workflow",
+    "inputs": {
+      "workflow_state": "mapping",
+      "is_background": "toggle",
+      "name": "text"
+    },
+   },
+   "end": {
+    "label": "End",
+    "description": "This is the end of the workflow",
+    "outputs": {
+      "output_mapping": "mapping",
+      "call_flow": "dropdown",
+      "name": "text"
+    },
+   }
 };
 
 export {blocksRequirements};
